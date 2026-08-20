eec5b968bff83e7d469f6cbb4b7ba80d3bcaa773
djjelian3 <djjelian3@gmail.com>
2026-08-20 09:02:17 +0800

    Delete iprime-vision directory

--- iprime-vision/routes/__init__.py
+++
@@ -1,9 +0,0 @@
-from .appointment_routes import appointment_bp
-from .auth_routes import auth_bp
-from .page_routes import page_bp
-
-
-def register_routes(app):
-    app.register_blueprint(auth_bp, url_prefix="/api/auth")
-    app.register_blueprint(appointment_bp, url_prefix="/api/appointments")
-    app.register_blueprint(page_bp)
--- iprime-vision/routes/page_routes.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, current_app
-
-
-page_bp = Blueprint("pages", __name__)
-
-
-@page_bp.get("/")
-def homepage():
-    return current_app.send_static_file("index.html")
